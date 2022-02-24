4cf020b496fc38f33532e3043514aae8da72180f
V radiulov <radiulovv.a@gmail.com>
2022-02-25 08:16:52 +1000

    empty

---
+++ app/src/main/java/com/example/myfirsttests/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myfirsttests
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myfirsttests.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+    }
+}
